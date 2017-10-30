ae0f2a56d99f8316469ca5be37eb9b2c1f502565
Hanxue <hanxue@outlook.com>
2017-10-30 23:01:02 +0800

    Update anypaste 1.0.0 (#2)
    - Update description
    - Remove caveat about --HEAD because it is no longer required

--- anypaste.rb
+++ anypaste.rb
@@ -1,22 +1,14 @@
 class Anypaste < Formula
-  desc "Upload and share any file with a single command."
+  desc "File sharing site aggregator for *nix-like systems"
   homepage "https://anypaste.xyz"
-  # url "https://raw.githubusercontent.com/markasoftware/anypaste/master/anypaste"
-  sha256 "1f0f2d5e201f1fdc1472f201f04430d809bf442ad034c194e70d8921823e990e"
+  url "https://github.com/markasoftware/anypaste/archive/v1.0.0.tar.gz"
+  sha256 "e7816d4df6a71627fe18bc3af2a29cfe1bfc4e84f124120ca48be90fdb89a674"
   head "https://github.com/markasoftware/anypaste.git"
 
   bottle :unneeded
 
   def install
     bin.install "anypaste"
-  end
-
-  def caveats
-    <<~EOS
-      There are no released versions of anypaste yet. To install
-
-          $ brew install --HEAD anypaste
-    EOS
   end
 
   test do
